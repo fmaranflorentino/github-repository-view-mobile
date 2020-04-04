73417c1509a6a9500ec307853e1851f80d621154
Flávio Florentino <florentino.flavioweb@gmail.com>
2020-04-04 10:25:05 -0300

    feat(app): app ready for progress

---
+++ src/index.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import {StatusBar} from 'react-native';
+
+import 'react-native-gesture-handler';
+
+import Routes from './routes';
+
+const App: () => React$Node = () => {
+  return (
+    <>
+      <StatusBar barStyle="light-content" backgroundColor="#7159c1" />
+      <Routes />
+    </>
+  );
+};
+
+export default App;
---
+++ src/routes.js
@@ -0,0 +1,33 @@
+import React from 'react';
+
+import {NavigationContainer} from '@react-navigation/native';
+import {createStackNavigator} from '@react-navigation/stack';
+
+import Main from './pages/Main';
+
+const Stack = createStackNavigator();
+
+const screenOptions = {
+  headerLayoutPreset: 'center',
+  headerBackTitleVisible: false,
+  headerStyle: {
+    backgroundColor: '#7159c1',
+  },
+  headerTintColor: '#fff',
+};
+
+function Routes() {
+  return (
+    <NavigationContainer>
+      <Stack.Navigator>
+        <Stack.Screen
+          name="Home"
+          component={Main}
+          options={screenOptions}
+        />
+      </Stack.Navigator>
+    </NavigationContainer>
+  );
+}
+
+export default Routes;
