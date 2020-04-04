d7b6e5ac023b447a463ed034badca6976faca381
Flávio Florentino <florentino.flavioweb@gmail.com>
2020-04-04 16:02:27 -0300

    feat(stars): listing stars

--- src/pages/User/index.js
+++ src/pages/User/index.js
@@ -3,7 +3,19 @@ import { Text, ActivityIndicator } from 'react-native';
 
 import api from '../../services/api';
 
-import { Container } from './styles';
+import {
+  Container,
+  Header,
+  Avatar,
+  Name,
+  Bio,
+  Stars,
+  Starred,
+  OwnerAvatar,
+  Info,
+  Title,
+  Author,
+} from './styles';
 
 export default function User({ route }) {
   const { params } = route;
@@ -11,6 +23,8 @@ export default function User({ route }) {
 
   const [userInfo, setUserInfo] = useState([]);
   const [loading, setLoading] = useState(false);
+
+  console.tron.log(user);
 
   useEffect(() => {
     async function loadUserInfo() {
@@ -20,7 +34,6 @@ export default function User({ route }) {
         const response = await api.get(`/users/${user.login}/starred`);
 
         setUserInfo(response.data);
-
 
         setLoading(false);
       } catch (error) {
@@ -33,7 +46,31 @@ export default function User({ route }) {
 
   return (
     <Container>
-      {loading ? <ActivityIndicator color="#7159c1" /> : <Text>Oi</Text>}
+      {loading ? (
+        <ActivityIndicator color="#7159c1" />
+      ) : (
+        <>
+          <Header>
+            <Avatar source={{ uri: user.avatar_url }} />
+            <Name>{user.name}</Name>
+            <Bio>{user.bio}</Bio>
+          </Header>
+
+          <Stars
+            data={userInfo}
+            keyExtractor={(star) => String(star.id)}
+            renderItem={({ item }) => (
+              <Starred>
+                <OwnerAvatar source={{ uri: item.owner.avatar_url }} />
+                <Info>
+                  <Title>{item.name}</Title>
+                  <Author>{item.owner.login}</Author>
+                </Info>
+              </Starred>
+            )}
+          />
+        </>
+      )}
     </Container>
   );
 }
